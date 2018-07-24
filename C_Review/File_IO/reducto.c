48cb5310c7837a61cc20e74e22604b40d062d498
QAlexBall <zhuderen12345@icloud.com>
2018-07-25 01:58:44 +0800

    update

--- C_Review/File_IO/reducto.c
+++ C_Review/File_IO/reducto.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
 	FILE *in, *out;		// 声明两个指向FILE的指针
 	int ch;
 	char name[LEN];		// 存储输出文件名
+	int count = 0;
 
 	/*检查命令行参数*/
 	if(argc < 2) {
@@ -23,6 +24,17 @@ int main(int argc, char *argv[]) {
 	strncpy(name, argv[1], LEN - 5);	// 拷贝文件名
 	name[LEN - 5] = '\0';
 	strcat(name, ".red"); 			// 在文件名后添加.red
+	if((out = fopen(name, "w")) == NULL) { // 以写模式打开
+		fprintf(stderr, "Can't create output file.\n");
+		exit(3);
+	}
+	/*拷贝数据*/
+	while((ch = getc(in)) != EOF) {
+	if(count++ % 3 == 0)
+		putc(ch, out);	// 打印第三个字符中的第一个字符
+	}
+	if(fclose(in) != 0 || fclose(out) != 0) 
+		fprintf(stderr, "Error in closing files\n");
 
 	return 0;
 }
