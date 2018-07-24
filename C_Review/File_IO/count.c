06ec2cde14411e6bff959f3bb59a04a204d53478
QAlexBall <zhuderen12345@icloud.com>
2018-07-24 23:24:15 +0800

    update

--- C_Review/File_IO/count.c
+++ C_Review/File_IO/count.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv []) {
 		printf("Usage: %s filename\n", argv[0]);
 		exit(EXIT_FAILURE); 
 	}
-	if((fp = fopen(argv[1], "r")) == NULL) {
+	if((fp = fopen(argv[1], "r")) == NULL) { 	// 打开文件并判断文件是否为NULL.
 		printf("Can't open %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
---
+++ C_Review/reducto.c
@@ -0,0 +1,23 @@
+// reducto.c -把文件压缩成原来的1/3
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 40
+
+int main(int argc, char *argv[]) {
+	FILE *in, *out;		// 声明两个指向FILE的指针
+	int ch;
+	char name[LEN];		// 存储输出文件名
+
+	// 检查命令行参数
+	if(argc < 2) {
+		fprintf(stderr, "Usage: %s filename\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	// 设置输入
+	if ((in = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "I couldn't open file the file \"%s\"\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	// 设置输出
+}
