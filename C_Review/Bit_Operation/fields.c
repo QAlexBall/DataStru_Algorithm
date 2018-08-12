b0af933fd1859771c53ec3ff36c52fb386d0db56
QAlexBall <zhuderen12345@icloud.com>
2018-08-12 22:24:19 +0800

    update

---
+++ C_Review/Bit_Operation/fields.c
@@ -0,0 +1,18 @@
+/* fields.c -- 定义并使用字段 */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+/* 线的样式 */
+#define SOLID 0
+#define DOTTED 1
+#define DASHED 2
+
+/* 三原色 */
+#define BLUE 4
+#define GREEN 2
+#define RED 1
+
+/* 混合色 */
+#define BLACK 0
+#define 
