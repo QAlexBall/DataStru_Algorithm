21d8b60bb49547da1ece4652826fcba052b0211b
QAlexBall <zhuderen12345@icloud.com>
2018-08-23 22:55:31 +0800

    update

--- DataStructure/Graph/Matrix_Graph.c
+++ DataStructure/Graph/Matrix_Graph.c
@@ -4,7 +4,11 @@
 // ----- 图的数组(邻接矩阵)存储表示 -----
 #define INFINITY INT_MAX		// 最大值
 #define MAX_VERTEXT_NUM 20		// 最大顶点个数
-typedef enum {DG, DN, UDG, UDN} // {有向图， 有向网， 无向图， 无向网}
+typedef enum {DG, DN, UDG, UDN} GraphKind; // {有向图， 有向网， 无向图， 无向网}
 typedef struct ArcCell {
 	VRType adj;			// VRType是顶点
-}
+
+        InfoType * info;
+}ArcCell, AdjMatrix[MAX_VERTEXT_NUM][MAX_VERTEXT_NUM];
+
+
